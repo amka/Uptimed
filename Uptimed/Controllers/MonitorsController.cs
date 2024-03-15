54e95718db98a5454f132bb0dccf64382154c556
Andrey Maksimov <andrey@tenderowl.com>
2024-03-15 10:18:48 +0300

    Add dosctrings

--- Uptimed/Controllers/MonitorsController.cs
+++ Uptimed/Controllers/MonitorsController.cs
@@ -18,11 +18,16 @@ public class MonitorsController(
     MonitorService monitorService,
     ILogger<MonitorsController> logger) : Controller
 {
-    // GET
+    /// <summary>
+    /// Controller for managing monitors.
+    /// </summary>
+    /// <param name="userManager">The user manager used for authorization.</param>
+    /// <param name="monitorService">The service used to manage monitors.</param>
+    /// <param name="logger">The logger used for logging.</param>
+    /// <response code="401">User is not authorized.</response>
+    /// <response code="400">User is not in the database.</response>
+    /// <response code="200">Returns a list of monitors and total count.</response>
     [HttpGet]
-    [ProducesResponseType<Monitor>(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetMonitorsAsync([FromQuery] int page = 0, [FromQuery] int pageSize = 10)
     {
         var user = await userManager.GetUserAsync(User);
@@ -33,18 +38,32 @@ public class MonitorsController(
         return Ok(new GetMonitorsResponse(monitors, total));
     }
 
+    /// <summary>
+    /// Retrieves a specific monitor by its id.
+    /// </summary>
+    /// <param name="id">The id of the monitor to retrieve.</param>
+    /// <response code="401">User is not authorized.</response>
+    /// <response code="404">Monitor with given id does not exist.</response>
+    /// <response code="200">Returns the monitor.</response>
     [HttpGet("{id}")]
     public async Task<IActionResult> GetMonitorAsync(string id)
     {
         var user = await userManager.GetUserAsync(User);
         if (user == null) return Unauthorized();
-        
+
         var monitor = await monitorService.GetUserMonitorAsync(user, id);
         if (monitor == null) return NotFound();
-        
+
         return Ok(monitor);
     }
 
+    /// <summary>
+    /// Creates a new monitor.
+    /// </summary>
+    /// <param name="request">The request containing the monitor information.</param>
+    /// <response code="401">User is not authorized.</response>
+    /// <response code="201">Returns the created monitor.</response>
+    /// <response code="400">Request is invalid.</response>
     [HttpPost]
     [Consumes(MediaTypeNames.Application.Json)]
     [ProducesResponseType<Monitor>(StatusCodes.Status201Created)]
@@ -65,7 +84,7 @@ public class MonitorsController(
             CreatedAt = DateTime.UtcNow,
             UpdatedAt = DateTime.UtcNow
         };
-        
+
         await monitorService.CreateMonitorAsync(monitor);
         return Created("CreateMonitorAsync", monitor);
     }
